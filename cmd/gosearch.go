8e9ff141683985b7a67b5d9435cd2795610f67e5
Artem <balusoff@gmail.com>
2022-04-14 01:41:05 +0300

    added gosearch.go

---
+++ cmd/gosearch.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"GoSearch/pkg/crawler"
+	"GoSearch/pkg/crawler/spider"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	//Инициализация аргументов
+	sFlag := flag.String("s", "", `поиск слова на страницах "go.dev" и "golang.org"`)
+	dFlag := flag.Int("d", 0, "глубина перехода по ссылкам")
+	flag.Usage = help
+	flag.Parse()
+
+	//Инициализация робота
+	spr := spider.New()
+
+	//Список адресов
+	adr := []string{"https://go.dev", "https://golang.org"}
+
+	//Результат сканированя робота
+	var data []crawler.Document
+
+	//Добавление результатов сканирования
+	for _, v := range adr {
+		res, err := spr.Scan(v, *dFlag)
+		if err != nil {
+			log.Printf("Ошибка! %v", err)
+		}
+		data = append(data, res...)
+	}
+
+	var urlC int //счетчик ссылок
+
+	//Поиск слова в найденных ссылках
+	for _, v := range data {
+		if strings.Contains(v.URL, *sFlag) {
+			fmt.Println(v.URL)
+			urlC++
+		}
+	}
+
+	fmt.Printf("Всего нaйдено ссылок: %d", urlC)
+
+}
+
+//help справка по использованию утилиты
+func help() {
+	_, _ = fmt.Fprintf(os.Stderr, "Использование:\n")
+	_, _ = fmt.Fprintf(os.Stderr, "  %s [аргументы]\n", filepath.Base(os.Args[0]))
+	_, _ = fmt.Fprintf(os.Stderr, "\n")
+
+	_, _ = fmt.Fprintf(os.Stderr, "Aргументы:\n")
+	flag.PrintDefaults()
+}
