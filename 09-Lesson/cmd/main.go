7b32b00e50019ba2517f8596ad8355cb9a897df5
Artem Balusov <a.balusov@aladdin.ru>
2022-05-18 20:12:57 +0300

    Task №2

--- 09-Lesson/cmd/main.go
+++ 09-Lesson/cmd/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	count "GoSearch/09-Lesson/pkg"
+	count "GoSearch/09-Lesson/pkg/counterparty"
+	icount "GoSearch/09-Lesson/pkg/icounterparty"
 	"fmt"
 )
 
 func main() {
+	// Task 1
+	a := icount.NewEmployee(25)
+	b := icount.NewCustomer(35)
+	c := icount.NewCustomer(35)
+	d := icount.NewEmployee(40)
+
+	fmt.Printf("Самому старшему контрагенту %d лет.\n", icount.Elder(a, b, c, d))
+
+	// Task 2
 	alice := count.NewEmployee(25)
 	bob := count.NewCustomer(35)
 	carol := count.NewCustomer(35)
 	dan := count.NewEmployee(40)
 
-	fmt.Printf("Самому старшему контрагенту %d лет.", count.Elder(alice, bob, carol, dan))
+	fmt.Printf("Самому старшему контрагенту %d лет.\n", count.Elder(alice, bob, carol, dan))
+
 }
---
+++ 09-Lesson/pkg/icounterparty/icounterparty.go
@@ -0,0 +1,40 @@
+// Package icounterparty implements functions for the manipulation of counterparties.
+package icounterparty
+
+type Employee struct {
+	age int
+}
+
+type Customer struct {
+	age int
+}
+
+type Age interface {
+	Age() int
+}
+
+func NewEmployee(age int) *Employee {
+	return &Employee{age: age}
+}
+
+func NewCustomer(age int) *Customer {
+	return &Customer{age: age}
+}
+
+func (e *Employee) Age() int {
+	return e.age
+}
+
+func (c *Customer) Age() int {
+	return c.age
+}
+
+func Elder(any ...Age) int {
+	var elder int
+	for _, a := range any {
+		if a.Age() > elder {
+			elder = a.Age()
+		}
+	}
+	return elder
+}
